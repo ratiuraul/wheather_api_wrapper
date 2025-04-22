d894f8812c5282269090440bd95fb02a33e96429
Raul <ratiuraul@github.com>
2025-04-22 07:51:17 +0300

    move redis url to .env

--- weather_api/extensions.py
+++ weather_api/extensions.py
@@ -1,18 +1,22 @@
 """
 Module to integrate 3rd party extensions to our app.
 """
+import os
+
+from dotenv import load_dotenv
 from flask_caching import Cache
 from redis import Redis
 
-
+load_dotenv()
 # Initialize Redis connection
 # Example Redis URL, change as needed
-redis = Redis.from_url("redis://localhost:6379/0")
+redis_url = os.getenv('REDIS_URL')
+redis = Redis.from_url(redis_url)
 
 # Initialize Flask-Caching with Redis as the cache backend
 cache = Cache(config={
     'CACHE_TYPE': 'redis',
-    'CACHE_REDIS_URL': 'redis://localhost:6379/0',  # Use the same Redis URL
+    'CACHE_REDIS_URL': redis_url,  # Use the same Redis URL
     # Set a default cache timeout (e.g., 5 minutes)
     'CACHE_DEFAULT_TIMEOUT': 300
 })
