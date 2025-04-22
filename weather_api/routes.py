14b418c5879e4b092b0ce3b70f17fdcb48e035df
Raul <ratiuraul@github.com>
2025-04-22 08:46:04 +0300

    handle errors and display usefull messages to users

--- weather_api/routes.py
+++ weather_api/routes.py
@@ -1,23 +1,39 @@
 """
 Define wheater API routes and View Functions
 """
-
 import json
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from requests.exceptions import HTTPError
 
 from weather_api.services import get_weather
 
 weather_bp = Blueprint('weather', __name__)
 
 
+def handle_client_errors(func):
+    """Decorator to handle errors and display usefull messages to clients."""
+    def wrapper(*args, **kwargs):
+        try:
+            response = func(*args, **kwargs)
+            return response
+        except HTTPError as http_error:
+            return jsonify({
+                "status": "error",
+                "message": f"External service error: \
+{http_error.response.status_code} - {http_error.response.text}"
+            }), http_error.response.status_code
+
+    return wrapper
+
+
 @weather_bp.route('/weather/<city>', methods=['GET'])
+@handle_client_errors
 def city_weather(city: str) -> json:
     """
     Call 3rd party api for given city name.
     :param city: name of the city to get weather data for
     :returns: json object with weather data
     """
-
     weather_data = get_weather(city)
     return weather_data.json()
